c67bd40ce5876d2b3d55f2d58b327a187e91c6d8
benmelnick <bmelnick3@gatech.edu>
2020-10-10 14:54:37 -0400

    get_spotify_uri implementation for searching for a song on Spotify using song name and artist

--- create_playlist.py
+++ create_playlist.py
@@ -35,9 +35,25 @@ class CreatePlaylist:
 
         return response_json["id"]
 
-    # Step 4: search for the song in spotify
-    def get_spotify_uri(self):
-        pass
+    """ 
+    Step 4: search for the song in spotify
+    """
+    def get_spotify_uri(self, song_name, artist):
+        query = SPOTIFY_API_URL + "/search?query=track%3A{}+artists%3A{}&type=track&offset=0&limit=20".format(
+            song_name,
+            artist
+        )
+        response = requests.get(
+            query,
+            headers=HEADERS
+        )
+        response_json = response.json()
+        songs = response_json["tracks"]["items"]
+
+        # use just the first song
+        uri = songs[0]
+
+        return uri
 
     # Step 5: add song to new playlist
     def add_song_to_playlist(self):
