aa76dea20d6e8524f197266da71dbb82822c4cc2
benmelnick <bmelnick3@gatech.edu>
2020-10-10 14:30:22 -0400

    Create playlist infra

---
+++ create_playlist.py
@@ -0,0 +1,24 @@
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    # Step 1: log into youtube
+    def get_youtube_client(self):
+        pass
+
+    # Step 2: get liked videos
+    def get_liked_videos(self):
+        pass
+
+    # Step 3: create a new playlist in spotify
+    def create_playlist(self):
+        pass
+
+    # Step 4: search for the song in spotify
+    def get_spotify_uri(self):
+        pass
+
+    # Step 5: add song to new playlist
+    def add_song_to_playlist(self):
+        pass
